29c23a8105ed56430323f85c42999593b948b762
Tushank Bhardwaj <tushankbhardwaj121@gmail.com>
2025-01-05 04:43:12 +0530

    product Item page

--- pages/product_page.jsx
+++ pages/product_page.jsx
@@ -18,59 +18,78 @@ function Product_item() {
   const location = useLocation();
   const productID = location.state?.productID;
 
-  console.log("Loaction:", location);
-  console.log("ID:", productID);
+  // console.log("Loaction:", location);
+  // console.log("ID:", productID);
 
   // Fetch images from the database
   useEffect(() => {
     const fetchData = async (id) => {
       try {
         const response = await axios.post(
-          `http://127.0.0.1:8000/collections/${id}`
+          `http://127.0.0.1:8000/productItem/${id}`
         );
         const response_data = response.data;
         setProductData(response_data);
-        console.log(response_data);
-        return response_data;
+        // console.log(response_data);
+        // return response_data;
       } catch (error) {
         console.error("Error fetching images:", error);
       }
     };
 
     fetchData(productID);
-  }, [productID]);
+  }, []);
 
   if (!productData) {
-    <div>...Loading...</div>;
+    return <div>...Loading...</div>;
   }
+
+  useEffect(() => {
+    if (productData?.Image?.length > 0) {
+      setSelectedImage(productData.Image[0].image_url);
+    }
+  }, [productData]);
 
   return (
     <>
       <Header />
       <div className="product_left_container">
-        <div className="product_left_container_left_img">
-          {images.map((image, index) => (
-            <div
-              className="product_left_container_left_img_sub"
-              key={index}
-              onClick={() => setSelectedImage(image)}
-            >
-              <img src={image} alt={`Image ${index + 1}`} />
-            </div>
-          ))}
-        </div>
+        {productData.Image && (
+          <div className="product_left_container_left_img">
+            {productData.Image.map((image) => (
+              <div
+                className="product_left_container_left_img_sub"
+                key={image.id}
+                onClick={() => setSelectedImage(image.image_url)}
+                style={{
+                  border:
+                    selectedImage === image.image_url
+                      ? "1px solid black"
+                      : "none",
+                }}
+              >
+                <img src={image.image_url} alt={`Product ${image.id}`} />
+              </div>
+            ))}
+          </div>
+        )}
         <div className="product_left_container_right_img">
           {selectedImage ? (
-            <img src={selectedImage} alt="1" />
+            <img src={selectedImage} alt="Selected product" />
           ) : (
             <p>Loading...</p>
           )}
         </div>
       </div>
       <div className="product_right_container">
-        <div className="product_right_container_name">
-          <h2>SMK TYPHOON SOLID GLOSS BLACK HELMET</h2>
-        </div>
+        {productData.Product && (
+          <div
+            className="product_right_container_name"
+            key={productData.Product.id}
+          >
+            <h2>{productData.Product.name.toUpperCase()}</h2>
+          </div>
+        )}
         <div className="product_right_container_review_price">
           <div className="product_right_container_review">
             <div className="product_right_container_review_star">
@@ -82,43 +101,53 @@ function Product_item() {
               <span>No reviews</span>
             </div>
           </div>
-          <div className="product_right_container_price">
-            <span>Rs. 4,950.00</span>
-          </div>
+          {productData.Product && (
+            <div
+              className="product_right_container_price"
+              key={productData.Product.id}
+            >
+              <span>Rs. {productData.Product.price}.00</span>
+            </div>
+          )}
           <div className="product_right_container_review_price_content">
             <span>MRP is inclusive of all taxes.</span>
           </div>
         </div>
         <div className="product_right_container_other_stuff">
-          <div className="product_right_container_other_stuff_color">
-            <div className="product_right_container_other_stuff_color_heading">
-              <h5>COLOR</h5>
+          {productData.Color && (
+            <div className="product_right_container_other_stuff_color">
+              <div className="product_right_container_other_stuff_color_heading">
+                <h5>COLOR</h5>
+              </div>
+              {productData.Color.map((col) => (
+                <div className="product_right_container_other_stuff_color_different">
+                  <div
+                    className="product_right_container_other_stuff_color_box"
+                    key={col.id}
+                  >
+                    <span>{col.available_colors.toUpperCase()}</span>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="product_right_container_other_stuff_color_different">
-              <div className="product_right_container_other_stuff_color_box">
-                <span>PURPLE</span>
+          )}
+          {productData.Size && (
+            <div className="product_right_container_other_stuff_size">
+              <div className="product_right_container_other_stuff_size_heading">
+                <h5>SIZE</h5>
+              </div>
+              <div className="product_right_container_other_stuff_size_different">
+                {productData.Size.map((size) => (
+                  <div
+                    className="product_right_container_other_stuff_size_box"
+                    key={size.id}
+                  >
+                    <span>{size.sizes}</span>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
-          <div className="product_right_container_other_stuff_size">
-            <div className="product_right_container_other_stuff_size_heading">
-              <h5>SIZE</h5>
-            </div>
-            <div className="product_right_container_other_stuff_size_different">
-              <div className="product_right_container_other_stuff_size_box">
-                <span>S</span>
-              </div>
-              <div className="product_right_container_other_stuff_size_box">
-                <span>M</span>
-              </div>
-              <div className="product_right_container_other_stuff_size_box">
-                <span>L</span>
-              </div>
-              <div className="product_right_container_other_stuff_size_box">
-                <span>XL</span>
-              </div>
-            </div>
-          </div>
+          )}
           <div
             className="product_right_container_other_stuff_size_chart"
             onClick={openModal}
